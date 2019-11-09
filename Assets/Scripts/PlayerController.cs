8dfa479a98fc0bb11c3c401cf157f6461a9262f3
Sven <sven-schaefer@gmx.net>
2019-11-09 19:41:09 +0100

    Added BigMonsta VFX to player.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,11 +4,12 @@ using UnityEngine.Experimental.VFX;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 300;
+    [SerializeField] private float scareMoveSpeedModifier = .5f;
     [SerializeField] private float maxDistanceAboveGround = 5f;
     [SerializeField] private float minDistanceAboveGround = 0f;
 
     [SerializeField, Range(.01f, .25f)] private float ghostVFXLerpSpeed = 0.01f;
-    //[SerializeField] private AnimationCurve ghostVFXLerpAnimCurve;
+    [SerializeField, Range(.01f, .25f)] private float bigMonstaLerpSpeed = 0.01f;
     private float terrainHeightUnderPlayer;
 
     private Rigidbody body;
@@ -20,16 +21,23 @@ public class PlayerController : MonoBehaviour
 
     private RaycastHit[] groundHits = new RaycastHit[1];
 
-    private VisualEffect ghostVFX;
+    [SerializeField] private VisualEffect ghostVFX;
+    [SerializeField] private VisualEffect bigMonsterVFX;
     private const string GHOST_ATTRACTIVETARGETPOSITION_NAME = "AttractiveTargetPosition";
+    private const string BIGMONSTERSCARE_EVENTNAME = "OnScare";
+    private const string BIGMONSTERSTOPSCARE_EVENTNAME = "StopScare";
+
+    public bool InScareMode { get; private set; }
+
+    private Vector3 bigMonstaStartPos;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody>();
         cam = FindObjectOfType<Cinemachine.CinemachineVirtualCamera>();
-        ghostVFX = transform.parent.GetComponentInChildren<VisualEffect>();
         Cursor.lockState = CursorLockMode.Confined;
         Cursor.visible = false;
+        ghostVFX.SendEvent(BIGMONSTERSTOPSCARE_EVENTNAME);
     }
 
     private void Update()
@@ -37,6 +45,21 @@ public class PlayerController : MonoBehaviour
         GetInput();
         CheckForGround();
         ghostVFX.SetVector3(GHOST_ATTRACTIVETARGETPOSITION_NAME, ghostVFX.transform.InverseTransformPoint(body.position));
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DoSomeScaring();
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            EndTheScaring();
+        }
+
+        if (InScareMode)
+        {
+            bigMonsterVFX.transform.position = Vector3.Lerp(bigMonsterVFX.transform.position, transform.position, bigMonstaLerpSpeed);
+        }
     }
 
     private void FixedUpdate()
@@ -44,6 +67,23 @@ public class PlayerController : MonoBehaviour
         HandleMovement();
         ghostVFX.transform.position = Vector3.Lerp(ghostVFX.transform.position, transform.position, ghostVFXLerpSpeed);
     }
+
+    private void DoSomeScaring()
+    {
+        bigMonsterVFX.SendEvent(BIGMONSTERSCARE_EVENTNAME);
+        ghostVFX.SendEvent(BIGMONSTERSCARE_EVENTNAME);
+        InScareMode = true;
+        bigMonstaStartPos = transform.position + (cam.transform.position - transform.position).normalized * 6f;
+        bigMonsterVFX.transform.position = bigMonstaStartPos;
+    }
+
+    private void EndTheScaring()
+    {
+        bigMonsterVFX.SendEvent(BIGMONSTERSTOPSCARE_EVENTNAME);
+        ghostVFX.SendEvent(BIGMONSTERSTOPSCARE_EVENTNAME);
+        InScareMode = false;
+    }
+
 
     private void GetInput()
     {
@@ -80,7 +120,7 @@ public class PlayerController : MonoBehaviour
     {
         if (moveVector.magnitude <= 0.01f)
             return;
-        Vector3 targetPos = body.position + moveVector.normalized * moveSpeed * Time.fixedDeltaTime;
+        Vector3 targetPos = body.position + moveVector.normalized * (moveSpeed * (InScareMode ? scareMoveSpeedModifier : 1f)) * Time.fixedDeltaTime;
         Vector3 newPosition = new Vector3(targetPos.x,
                                           Mathf.Clamp(targetPos.y,
                                                       terrainHeightUnderPlayer + minDistanceAboveGround,
