6769e0bffebd06983bfdd037ce03c386de1d0cdb
Sven <sven-schaefer@gmx.net>
2019-11-10 13:31:29 +0100

    Added heart to bryce prefab.

--- Assets/Scripts/BryceHeartBeat.cs
+++ Assets/Scripts/BryceHeartBeat.cs
@@ -5,10 +5,24 @@ public class BryceHeartBeat : MonoBehaviour
 {
     [Range(60, 180)] public int bpm = 60;
     [SerializeField] private AnimationCurve heartBeatAnimCurve;
+    [SerializeField] private Material mat;
+
+    private float heartVisibility;
+    public float HeartVisibility
+    {
+        get { return heartVisibility; }
+        set
+        {
+            heartVisibility = Mathf.Clamp01(value);
+            mat.SetFloat("_UpperBodyMinAlpha", heartVisibility);
+        }
+    }
 
     private float AnimationLength => 60f / bpm;
 
     private Vector3 targetScale = Vector3.one * 2f;
+
+    public bool alive = true;
 
     private void Start()
     {
@@ -17,7 +31,7 @@ public class BryceHeartBeat : MonoBehaviour
 
     private IEnumerator HeartBeatRoutine()
     {
-        while (true)
+        while (alive)
         {
             yield return StartCoroutine(transform.ScaleRoutine(targetScale, AnimationLength, heartBeatAnimCurve));
         }
