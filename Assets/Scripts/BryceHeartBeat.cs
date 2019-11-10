2102c1a5476402c1296ecc83628888166360b84f
Sven <sven-schaefer@gmx.net>
2019-11-10 13:04:27 +0100

    Costum bryce shader and mat and bryce heart prefab.

---
+++ Assets/Scripts/BryceHeartBeat.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class BryceHeartBeat : MonoBehaviour
+{
+    [Range(60, 180)] public int bpm = 60;
+    [SerializeField] private AnimationCurve heartBeatAnimCurve;
+
+    private float AnimationLength => 60f / bpm;
+
+    private Vector3 targetScale = Vector3.one * 2f;
+
+    private void Start()
+    {
+        StartCoroutine(HeartBeatRoutine());
+    }
+
+    private IEnumerator HeartBeatRoutine()
+    {
+        while (true)
+        {
+            yield return StartCoroutine(transform.ScaleRoutine(targetScale, AnimationLength, heartBeatAnimCurve));
+        }
+    }
+}
