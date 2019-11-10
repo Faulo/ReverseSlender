8f7254788112fb9b8337f4d1fb5316fc87f32b3b
Daniel Schulz <faulolio@gmail.com>
2019-11-10 16:30:32 +0100

    make Bryce hide more, flee less

--- Assets/Scripts/AI/AgentSettings.cs
+++ Assets/Scripts/AI/AgentSettings.cs
@@ -23,9 +23,11 @@ namespace ReverseSlender.AI {
         public float verticalFieldOfView;
 
         [Header("Player Interaction")]
-        [Range(0, 1000), Tooltip("How much ghost-form scares Bryce.")]
+        [Range(0, 1000), Tooltip("How much Bryce scares ghost form.")]
+        public float stunMultiplier;
+        [Range(0, 1000), Tooltip("How much ghost form scares Bryce.")]
         public float ghostMultiplier;
-        [Range(0, 1000), Tooltip("How much monster-form scares Bryce.")]
+        [Range(0, 1000), Tooltip("How much monster form scares Bryce.")]
         public float monsterMultiplier;
         [Range(0, 2), Tooltip("How much collecting gems restores sanity.")]
         public float collectibleRest;
@@ -39,13 +41,13 @@ namespace ReverseSlender.AI {
         public AnimationCurve bpmOverFear;
 
         [Header("Flee Point Generation")]
-        [Tooltip("Whether or not to flee at all when spotting the player in monster-form.")]
+        [Tooltip("Whether or not to flee at all when spotting the player in monster form.")]
         public bool useFleePoints;
         [Tooltip("Prefab to spawn when fleeing.")]
         public Transform fleePointPrefab;
-        [Range(0, 100), Tooltip("Minimum distance to flee when spotting monster-form.")]
+        [Range(0, 100), Tooltip("Minimum distance to flee when spotting monster form.")]
         public float fleePointMinDistance;
-        [Range(0, 100), Tooltip("Maximum distance to flee when spotting monster-form.")]
+        [Range(0, 100), Tooltip("Maximum distance to flee when spotting monster form.")]
         public float fleePointMaxDistance;
         [Range(0, 10), Tooltip("How much fleeing should resemble a straight line away from the player.")]
         public float fleePointTurnabout;
